3969045bbe55f0de8c73cb9f0d6c9734c7f42a3e
knight <liuxiaopeng731@gmail.com>
2026-04-30 15:00:54 +0800

    update Arch init.md

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a28c6da3.js',
+                path_to_root + 'searchindex-7eda5832.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
